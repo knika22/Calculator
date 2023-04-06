a775e01f0d5e4a7c4a3dabdb3d37ffa466aa3baf
knika22 <knika22@epoka.edu.al>
2023-04-06 23:34:34 +0200

    added them all

--- main.c
+++ main.c
@@ -37,7 +37,33 @@ double PowerOfNum(double a,double b)
 {
     return pow(a,b);
 }
+double get_avg()
+{
+    int cnt=0;
+    double sum=0;
+    double num;
+    printf("Enter numbers to calculate average.Enter 0 to quit:\n");
+   do{
+    scanf("%lf", &num);
+    if(num!=0)
+    {
+        cnt++;
+        sum+=num;
+    }
+   }
+    while(num !=0);
 
+    if(cnt==0)
+    {
+        printf("No numbers entered.\n");
+        return 0;
+    }
+    else{
+        double average= sum/cnt;
+        printf("Average: %.2lf\n",average);
+        return average;
+    }
+}
 int main()
 {
     double a,b,result;
@@ -85,6 +111,10 @@ int main()
        result=PowerOfNum(a,b);
        printf("%.2lf  ",result);
 
+    }
+    if(symbol=='a')
+    {
+        get_avg();
     }
 
     return 0;
