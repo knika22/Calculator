56fed7477d0a4190c465c3b0b2c79dd7a9581438
knika22 <knika22@epoka.edu.al>
2023-04-07 13:36:48 +0200

    adding the code for the calculator

--- main.c
+++ main.c
@@ -37,33 +37,7 @@ double PowerOfNum(double a,double b)
 {
     return pow(a,b);
 }
-double get_avg()
-{
-    int cnt=0;
-    double sum=0;
-    double num;
-    printf("Enter numbers to calculate average.Enter 0 to quit:\n");
-   do{
-    scanf("%lf", &num);
-    if(num!=0)
-    {
-        cnt++;
-        sum+=num;
-    }
-   }
-    while(num !=0);
 
-    if(cnt==0)
-    {
-        printf("No numbers entered.\n");
-        return 0;
-    }
-    else{
-        double average= sum/cnt;
-        printf("Average: %.2lf\n",average);
-        return average;
-    }
-}
 int main()
 {
     double a,b,result;
@@ -112,10 +86,7 @@ int main()
        printf("%.2lf  ",result);
 
     }
-    if(symbol=='a')
-    {
-        get_avg();
-    }
+
 
     return 0;
 }
